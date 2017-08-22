aba0de016e1925f6ecf36903296856b3424a484c
Gavin Kyte <gpkyte@gmail.com>
2017-08-22 17:35:50 -0400

    Week 1 Setup

---
+++ week1.cpp
@@ -0,0 +1,19 @@
+//Week 1A
+// Gavin Kyte
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  // system("pause");
+  int i = 5;
+  if (10){
+    cout << "YES!" << endl;
+  }
+
+  // JAVA has true and false
+  // C++ has 0 and !0
+
+  return 0;
+}
