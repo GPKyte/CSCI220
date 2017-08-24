ab384f5123fd9e137292dd9c184cf46e6f9d05b7
Gavin Kyte <gpkyte@gmail.com>
2017-08-24 13:12:36 -0400

    Wrap up last session

--- week1.cpp
+++ week1.cpp
@@ -2,18 +2,23 @@
 // Gavin Kyte
 #include <stdio.h>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+// JAVA has true and false
+// C++ has 0 and !0
 int main() {
   std::cout << "Hello World" << std::endl;
   // system("pause");
   int i = 5;
+  float f = .6;
+  double d = .445;
+
+  cout << setw(5) << i << setw(5) << f << setw(7) << d << endl;
   if (10){
     cout << "YES!" << endl;
   }
 
-  // JAVA has true and false
-  // C++ has 0 and !0
 
   return 0;
 }
