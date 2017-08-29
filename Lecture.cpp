f7678e0db311f51f89c884faaa84260234925300
Gavin Kyte <gpkyte@gmail.com>
2017-08-29 14:00:07 -0400

    Talk about pointers

--- Lecture.cpp
+++ Lecture.cpp
@@ -10,28 +10,18 @@ using namespace std;
 template <class T> T getValueInRange(T, T);
 
 int main() {
-  int a = 6; int b = 25;
-  float c = 1.0987; float d = 102.938;
-  int res1 = getValueInRange(a, b);
-  float res2 = getValueInRange(c, d);
-  cout << res1 << endl;
-  cout << res2 << endl;
-  return 0;
-}
+  int i = 12;
+  // Pointer to type "int" p = the memory location of i
+  int * p = &i; // * is pointer, & is memory location
+  //?? hmm not sure here
+  int & r = i;
 
-template <class T> T getValueInRange(T low, T high){
-  while (1) {
-    cout << "Enter a value between " << low << " and " << high << ": " ;
-    cout.flush();
-    // Good practice to initialize values that aren't immediately overwritten.
-    //    Bc cpp grabs open memory that could have any value.
-    T value;
-    // Not error checking
-    cin >> value;
-    if (value >= low && value <= high){
-      return value;
-    } else {
-      cout << "Invalid" << endl;
-    }
-  }
+  // "Go to this pointer location and assign value 54 to it"
+  *p = 54;
+
+  cout << i << endl;
+  cout << hex << p << dec << endl;
+  cout << r << endl;
+  cout << * p << endl;
+  return 0;
 }
