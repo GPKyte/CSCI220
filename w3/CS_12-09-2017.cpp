879d3ca9c58a4033a2f368a1a078b93b18639ae2
Gavin Kyte <gpkyte@gmail.com>
2017-09-12 16:15:49 -0400

    Notes on Big-O and & vs *

---
+++ w3/CS_12-09-2017.cpp
@@ -0,0 +1,29 @@
+// Gavin Kyte
+// Tue Sep 12 13:18:34 2017
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Notes:
+// Declaration statement == "When declaring a type and name"
+//   Inside of declaration:
+//   int i; Create an int i at some point in memory
+//   float *f; Create a float type pointer
+//   Outside of declaration
+//   cout << *f "Follow pointer to it's value" == "Dereferencing"
+//   cout << &f "Give me memory address of f"
+// You can pass by:
+//   * value
+//   * reference
+// Big-O Definition (It's the upperbound)
+// f(n)=O(g(n)) there exist positive constants c and n0 such that
+//    0 <= f(n) <= cg(n) for all n >= n0
+//
+// Big-O Omega is the same, but reverse. It's lower bound
+// Big-O Theta is a tight bound, where inside upper and lower bounds
+//   Need to show that inequality is true for any true range of n values
+
+int main(){
+  return 0;
+}
