37e8d6dd1951db3e1c5cb8dcb933eac928eb6368
Gavin Kyte <gpkyte@gmail.com>
2017-12-04 11:34:00 -0500

    Spacing fix & change MAXHASH to prime

--- project4/HashTable.h
+++ project4/HashTable.h
@@ -1,26 +1,27 @@
 #pragma once
 #include "Record.h"
-#define MAXHASH 1000
-template <class T> class HashTable
-{
+#define MAXHASH 1009
+
+template <class T> class HashTable {
 private:
   Record<T> *hashMap;
   int currentSize;
-	//friend function for << operator.
-	friend ostream& operator<<(ostream& os, const HashTable& me) {
-		for (int i = 0; i < MAXHASH; i++) {
+  //friend function for << operator.
+  friend ostream& operator<<(ostream& os, const HashTable& me) {
+    for (int i = 0; i < MAXHASH; i++) {
       cout<<me.hashMap[i]<<endl;
     }
-		return os;
-	}
+    return os;
+  }
 
 public:
-	HashTable();
-	bool find(int key, T& value);
-	bool insert(int key, T value, int& collisions);
-	float alpha();
-	bool remove(int key);
-	int hashFunction(int key);
-	int probeFunction(int key);
-	~HashTable();
+  HashTable();
+  bool find(int key, T& value);
+  bool insert(int key, T value, int& collisions);
+  float alpha();
+  bool remove(int key);
+  unsigned hashFunction(int key);
+  unsigned probeFunction(int key);
+  unsigned probeFunction(int key, int count);
+  ~HashTable();
 };
